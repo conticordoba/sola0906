6e0f2668e51943cac1578300fe1599d39bf1cac6
conticordoba <cordobasantillan.maria@iesconcepcion.edu.ar>
2025-06-09 18:05:30 -0300

    ejercicio nuevo

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-console.log("Hola mundo")
+// en la compu elige un numero del 1 al 16
+// con una variable determinar si es igual al numero
+// si es igual mostrar un mensaje
 
-const a = 5
-const b = 10
-const c = a + b
-console.log(c)
-console.log ("el resultado es:" + c)
+const numeroElegido = 7;
+const numeroUsuario = parseInt(prompt("elige un numero del 1 al 16"));
+if (numeroUsuario === numeroElegido) {
+    console.log("¡feliidades! el numero es correcto")
+} 
+else {
+    console.log("el numero es insorrecto, vuelve a intentarlo");
+}
