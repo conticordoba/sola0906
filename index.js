22b0e296aa043513d1554bfd5cc251e5fa484d50
conticordoba <cordobasantillan.maria@iesconcepcion.edu.ar>
2025-06-09 18:22:09 -0300

    cambio de ejercitacion

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 // con una variable determinar si es igual al numero
 // si es igual mostrar un mensaje
 
-const numeroElegido = 7;
-const numeroUsuario = parseInt(prompt("elige un numero del 1 al 16"));
-if (numeroUsuario === numeroElegido) {
-    console.log("¡feliidades! el numero es correcto")
-} 
+//const numeroElegido = 7;
+//const numeroUsuario = parseInt(prompt("elige un numero del 1 al 16"));
+//if (numeroUsuario === numeroElegido) {
+//    console.log("¡feliidades! el numero es correcto")
+//} 
+//else {
+//    console.log("el numero es insorrecto, vuelve a intentarlo");
+//}
+
+//PEDIR LA EDAD DE UNA PERSONA Y DECIR
+//SI PUEDE VOTAR O NO
+
+const edad = parseInt(prompt("Ingrese su edad:"));
+if (edad >= 18) {
+    console.log("puede votar");
+}
 else {
-    console.log("el numero es insorrecto, vuelve a intentarlo");
+    console.log("no puede votar");
 }
